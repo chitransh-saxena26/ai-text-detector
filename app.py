62ef2a1c8803e74aed66212dea4d86aa3b19778b
Chitransh Saxena <saxenachitransh929@gmail.com>
2025-03-20 12:17:31 +0530

    Fixed flask

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from transformers import pipeline
 from flask_cors import CORS
@@ -32,4 +33,5 @@ def analyze():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
